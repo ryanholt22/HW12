0c02818c6d788946c326a4319d291c880ed31ae3
Ryan Holt <ryanholt22@yahoo.com>
2019-10-01 23:33:24 -0500

    PLEASE HEROKU LET ME TURN MY HW IN THX

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,8 +13,8 @@ app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
-app.get('/', function(req, res){
-  res.redirect('/todo');
+app.get("/", function(req, res) {
+  res.json(path.join(__dirname, "/index.html"));
 });
 
 connection.connect(function (err) {
